ab01c325a685e25d80081f97446e7db1a5595ea9
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-02-13 05:38:03 +0000

    Book Start3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'tops/start'
+ root to: 'tops#start'
+
   resources :books
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
